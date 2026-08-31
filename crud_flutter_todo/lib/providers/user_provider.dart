13d1412f93aee2c2c58df39d222cfa404fdbb4c3
Shrudhir Praveen <shrudhir007@gmail.com>
2026-08-31 23:58:14 +0100

    changing to notifier

--- crud_flutter_todo/lib/providers/user_provider.dart
+++ crud_flutter_todo/lib/providers/user_provider.dart
@@ -1,6 +1,19 @@
 import 'package:crud_flutter_todo/user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final usersProvider = Provider<List<User>>((ref) {
-  return users;
-});
+// final usersProvider = Provider<List<User>>((ref) {
+//   return users;
+// });
+
+class UsersNotifier extends Notifier<List<User>>{
+  @override
+
+  //to initialize state 
+  List<User>build(){
+    return users;
+  }
+}
+
+final usersProvider = NotifierProvider<UsersNotifier, List<User>>(
+  UsersNotifier.new
+)
